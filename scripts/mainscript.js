ddbd7d7f481570381876fc7aba808e7be6e8a7c3
Firsty <fc.firsty@gmail.com>
2026-03-07 12:06:38 +0700

    Add files via upload

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,1 +1,13 @@
-alert("Thanks for entering this site!")
+alert("Click/Tap anywhere to get full screen!")
+
+let Bool = true;
+
+document.addEventListener("click", function() {
+    if (Bool) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    };
+
+    Bool = !Bool;
+});
